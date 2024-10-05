8bc94e0e95ef022fd6d590181862706015891bf3
Jéssica Trigo <jessica.trigo05@icloud.com>
2024-10-05 14:41:21 +0200

    Initial Commit

---
+++ Exosky/ExoskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoskyApp.swift
+//  Exosky
+//
+//  Created by Jéssica Trigo on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExoskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
